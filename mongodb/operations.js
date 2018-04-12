b257d7a377e0d291449edd88cd7c68fc2f0405e2
Markus Bähr <markus.baehr@fh-stralsund.de>
2018-04-12 22:17:56 +0100

    Hotfix

--- mongodb/operations.js
+++ mongodb/operations.js
@@ -9,168 +9,156 @@ const handler = require('./handler');
 
 module.exports = {
 
-    /**********************************************
+    /***************************************************************************
      * Misc Operations
-     **********************************************/
+     **************************************************************************/
 
     /**
      * Generiert ein Sessiontoken
      */
     generateToken: function () {
         var returnObj = String(Math.floor(Math.random() * 10000)).padStart(4, "0");
-        //var returnObj = ("0000" + Math.floor(Math.random() * 10000)).slice(-4);
+        // var returnObj = ("0000" + Math.floor(Math.random() *
+        // 10000)).slice(-4);
         logging.Info("Token " + returnObj + " generated");
         return returnObj;
     },
 
-    /**********************************************
+    /***************************************************************************
      * Object Operations
-     **********************************************/
+     **************************************************************************/
 
     /**
-     * ToDo: bei pObject prüfen, ob es sich um eine _id handelt
-     * 
-     * sucht nach einem Objekt in der Datenbank und gibt dieses im Erfolgsfall zurück.
-     * Wurde kein Objekt übergeben, werden Alle Objekte der Collection zurückgegeben.
-     * pCollection -->
-     * pObject -->
-     * pCallback -->
+     * Objekt in der Datenbank und gibt dieses im Erfolgsfall zurück. Wurde kein
+     * Objekt übergeben, werden Alle Objekte der Collection zurückgegeben.
+     * pCollection --> pObject --> pCallback -->
      */
     findObject: function (pCollection, pObject, pCallback) {
-    	var database = mongo.Object();
-    	database.Client.connect(database.Url, function(error, result) {
-    		if (error) {
-    			pCallback(error, null)
-    		} else {
-    			if (pObject) {
-		    		result.db(database.Database).collection(pCollection).findOne(handler.idFriendlyQuery(pObject), function (err, db) {
-		    			result.close();
-		                pCallback(err, db);
-		            });     
-    			} else {
-    	    		result.db(database.Database).collection(pCollection).find({}).toArray(function (err, db) {
-    	    			result.close();
-    	                pCallback(err, db);
-    	            });       				
-    			}
-    		}
-    	});    	
+      var database = mongo.Object();
+      database.Client.connect(database.Url, function(error, result) {
+        if (error) {
+          pCallback(error, null);
+        } else {
+          if (pObject) {
+            result.db(database.Database).collection(pCollection).findOne(handler.idFriendlyQuery(pObject), function (err, db) {
+              result.close();
+              pCallback(err, db);
+            });
+          } else {
+            result.db(database.Database).collection(pCollection).find({}).toArray(function (err, db) {
+              result.close();
+              pCallback(err, db);
+            });
+          }
+        }
+      });
     },
 
     /**
-     * ---
-     * pCollection -->
-     * pObject -->
+     * --- pCollection --> pObject -->
      */
     updateObject: function (pCollection, pObject, pQuery, pCallback) {
-    	var database = mongo.Object();
-    	database.Client.connect(database.Url, function(error, result) {
-    		if (error) {
-    			pCallback(error, null)
-    		} else {    		
-    			var friendlyObject = handler.idFriendlyQuery(pObject);
-	    		result.db(database.Database).collection(pCollection).findAndModify(friendlyObject, [['_id', 'asc']],
-	                    {'$set': ((pQuery) ? pQuery : friendlyObject)}, {new: true, upsert: true}, function (err, db) {
-	                result.close();
-	                pCallback(err, db);
-	            });    
-    		}
-    	});
+      var database = mongo.Object();
+        database.Client.connect(database.Url, function(error, result) {
+          if (error) {
+            pCallback(error, null);
+          } else {
+            var friendlyObject = handler.idFriendlyQuery(pObject);
+            result.db(database.Database).collection(pCollection).findAndModify(friendlyObject, [['_id', 'asc']],{'$set': (pQuery ? pQuery : friendlyObject)}, {new: true, upsert: true}, function (err, db) {
+              result.close();
+              pCallback(err, db);
+            });
+          }
+        });
     },
 
     /**
-     * Entfernt Objekt aus der Collection pCollection
-     *
-     * return true, wenn löschen erfolgreich
-     *        false, wenn löschen nicht erfolgreich
+     * Entfernt Objekt aus der Collection pCollection return true, wenn löschen
+     * erfolgreich false, wenn löschen nicht erfolgreich
      */
     deleteObjects: function (pCollection, pObjects, pCallback) {
-    	var database = mongo.Object();
-    	database.Client.connect(database.Url, function(error, result) {
-    		if (error) {
-    			pCallback(error, null)
-    		} else {    		
-	    		result.db(database.Database).collection(pCollection).deleteMany(handler.idFriendlyQuery(pObjects), function (err, db) {
-	    			result.close();
-	                pCallback(err, db);
-	            });       		
-    		}
-    	});
+      var database = mongo.Object();
+      database.Client.connect(database.Url, function(error, result) {
+        if (error) {
+          pCallback(error, null);
+        } else {
+          result.db(database.Database).collection(pCollection).deleteMany(handler.idFriendlyQuery(pObjects), function (err, db) {
+            result.close();
+            pCallback(err, db);
+          });
+        }
+      });
     },
 
-    /**********************************************
+    /***************************************************************************
      * Collection Operations
-     **********************************************/
+     **************************************************************************/
 
     /**
-     * Erstellt eine Collection die der Datenbank zugefügt wird.
-     *
-     * pCollection -> Name der neuen Datenbank-Collection
+     * Erstellt eine Collection die der Datenbank zugefügt wird. pCollection ->
+     * Name der neuen Datenbank-Collection
      */
     createCollection: function (pCollection, pCallback) {
-    	var database = mongo.Object();
-    	database.Client.connect(database.Url, function(error, result) {    	
-    		if (error) {
-    			pCallback(error, null)
-    		} else {    		
-	    		result.db(database.Database).createCollection(pCollection, function (err, db) {
-	    			result.close();
-	                pCallback(err, db);
-	            });
-    		}
-    	});
+      var database = mongo.Object();
+      database.Client.connect(database.Url, function(error, result) {
+        if (error) {
+          pCallback(error, null);
+        } else {
+          result.db(database.Database).createCollection(pCollection, function (err, db) {
+            result.close();
+            pCallback(err, db);
+          });
+        }
+      });
     },
 
     /**
-     * Sucht nach einer bestimmten collection und gibt diese im Erfolgsfall zurück.
-     * Wurde keine Identifizierung übergeben, so werden alle verfügbaren Collections zurückgegeben.
-     * pCollection -->
-     * pCallback -->
+     * Sucht nach einer bestimmten collection und gibt diese im Erfolgsfall
+     * zurück. Wurde keine Identifizierung übergeben, so werden alle verfügbaren
+     * Collections zurückgegeben. pCollection --> pCallback -->
      */
     getCollection: function (pCollection, pCallback) {
-    	var database = mongo.Object();    	
-    	if (pCollection) {
-        	database.Client.connect(database.Url, function(error, result) {
-        		if (error) {
-        			pCallback(error, null)
-        		} else {        		
-	        		result.db(database.Database).collection(pCollection).find({}).toArray(function (err, db) {
-	        			result.close();
-	                    pCallback(err, db);
-	                });       		
-        		}
-        	});    		
-    	} else {
-        	database.Client.connect(database.Url, function(error, result) {
-        		if (error) {
-        			pCallback(error, null)
-        		} else {        		
-	        		result.db(database.Database).listCollections().toArray(function (err, db) {
-	        			result.close();
-	                    pCallback(err, db);
-	                });  
-        		}
-        	});
-    	}
+      var database = mongo.Object();
+        if (pCollection) {
+          database.Client.connect(database.Url, function(error, result) {
+            if (error) {
+              pCallback(error, null);
+            } else {
+              result.db(database.Database).collection(pCollection).find({}).toArray(function (err, db) {
+                result.close();
+                pCallback(err, db);
+              });
+            }
+          });
+        } else {
+          database.Client.connect(database.Url, function(error, result) {
+            if (error) {
+              pCallback(error, null);
+            } else {
+              result.db(database.Database).listCollections().toArray(function (err, db) {
+                result.close();
+                pCallback(err, db);
+              });
+            }
+          });
+        }
     },
 
     /**
-     * Entfernt eine übergebene Collection aus der Datenbank
-     *
-     * pCollection -> Name der zu löschenden Datenbank
-     * pCallback -->
+     * Entfernt eine übergebene Collection aus der Datenbank pCollection -> Name
+     * der zu löschenden Datenbank pCallback -->
      */
     dropCollection: function (pCollection, pCallback) {
-    	var database = mongo.Object();
-    	database.Client.connect(database.Url, function(error, result) {
-    		if (error) {
-    			pCallback(error, null)
-    		} else {    		
-	    		result.db(database.Database).servercollection(pCollection).drop(function (err, db) {
-	    			result.close();
-	                pCallback(err, db);
-	            });     
-    		}
-    	});
+      var database = mongo.Object();
+      database.Client.connect(database.Url, function(error, result) {
+        if (error) {
+          pCallback(error, null);
+        } else {
+          result.db(database.Database).servercollection(pCollection).drop(function (err, db) {
+            result.close();
+            pCallback(err, db);
+          });
+        }
+      });
     }
-}
+};
--- routes/test.js
+++ routes/test.js
@@ -9,12 +9,8 @@ var router = express.Router();
 /* GET */
 /* Find User(s) */
 router.get('/find/:collection', function(req, res) {
-    operations.findObject(req.params.collection, (handler
-	    .checkIfValidQuery(req.query) ? req.query : null), function(err,
-	    item) {
-	handler.dbResult(err, res, item, "Das Item "
-		+ JSON.stringify(req.query).replace(/\"/g, '')
-		+ " existiert nicht.");
+    operations.findObject(req.params.collection, (handler.checkIfValidQuery(req.query) ? req.query : null), function(err, item) {
+        handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " existiert nicht.");
     });
 });
 
@@ -22,51 +18,41 @@ router.get('/find/:collection', function(req, res) {
 /* Insert User */
 router.post('/insert/:collection', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-	operations.updateObject(req.params.collection, req.query, null,
-		function(err, item) {
-		    handler.dbResult(err, res, item, "Das Item "
-			    + JSON.stringify(req.query).replace(/\"/g, '')
-			    + " kann nicht hinzugefüt werden.");
-		});
+        operations.updateObject(req.params.collection, req.query, null, function(err, item) {
+            handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " kann nicht hinzugefüt werden.");
+        });
     } else {
-	res.status(422).jsonp({
-	    "error" : "Die übergebenen Parameter sind ungültig"
-	});
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
     }
 });
 
 /* Update User */
 router.post('/update/:collection/:id', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-	operations.updateObject(req.params.collection, handler
-		.idFriendlyQuery({
-		    _id : req.params.id
-		}), req.query, function(err, item) {
-	    handler.dbResult(err, res, item, "Das Item " + req.params.id
-		    + " konnte nicht mit "
-		    + JSON.stringify(req.query).replace(/\"/g, '')
-		    + " geupdatet werden.");
-	})
+        operations.updateObject(req.params.collection, handler.idFriendlyQuery({
+            _id : req.params.id
+        }), req.query, function(err, item) {
+            handler.dbResult(err, res, item, "Das Item " + req.params.id + " konnte nicht mit " + JSON.stringify(req.query).replace(/\"/g, '') + " geupdatet werden.");
+        })
     } else {
-	res.status(422).jsonp({
-	    "error" : "Die übergebenen Parameter sind ungültig"
-	});
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
     }
 });
 
 /* Delete User(s) */
 router.post('/delete/:collection', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-	operations.deleteObjects(req.params.collection, req.query, function(
-		err, item) {
-	    handler.dbResult(err, res, item, "Die Items mit den Eigenschaften "
-		    + JSON.stringify(req.query).replace(/\"/g, '')
-		    + " konnten nicht gelöscht werden.");
-	});
+        operations.deleteObjects(req.params.collection, req.query, function(err, item) {
+            handler.dbResult(err, res, item, "Die Items mit den Eigenschaften " + JSON.stringify(req.query).replace(/\"/g, '') + " konnten nicht gelöscht werden.");
+        });
     } else {
-	res.status(422).jsonp({
-	    "error" : "Die übergebenen Parameter sind ungültig"
-	});
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
     }
 });
 

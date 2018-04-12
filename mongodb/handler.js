132dad9179bff5d73dac3353d8c7e319073c2534
Markus Bähr <markus.baehr@fh-stralsund.de>
2018-04-12 21:25:45 +0100

    Cleanup Code

--- mongodb/handler.js
+++ mongodb/handler.js
@@ -1,43 +1,42 @@
-const logging = require('./logging'); 
+const logging = require('./logging');
 const ObjectID = require('mongodb').ObjectID;
 
 module.exports = {
-	idFriendlyQuery: function(query) {
-		var testQuery = query;
-		
-		if (!testQuery)
-			return testQuery;
-		
-		if (testQuery instanceof Array) {
-			for (var i = 0; testQuery.length > i; i += 1) {		
-				if (testQuery[i].hasOwnProperty('_id')) {
-					testQuery[i]["_id"] = new ObjectID(testQuery[i]["_id"]);
-				}		
-			}
-		} else {
-			if (testQuery.hasOwnProperty('_id')) {
-				testQuery["_id"] = new ObjectID(testQuery["_id"]);
-			}		
-		}
-		
-		return testQuery;
-	},
-	checkIfValidQuery: function(query) {
-		return (!query.hasOwnProperty(undefined) && Object.keys(query).length > 0);
-	},
-	JSONparse: function(property, value) {
-		return JSON.parse('{ "' + property + '": "' + value + '" }');
-	},
-	dbResult: function(err, res, result, resultType) {
-		if (err) {
-	    	logging.Error(err);
-	        res.status(422).jsonp({"error": "Es ist ein Fehler aufgetreten beim Versuch sich mit der Datenbank zu verbinden!"});
-	    }
-	    else {
-	    	if (!result)
-	    		res.status(422).jsonp({"error": resultType });
-	    	else
-	            res.status(200).jsonp(result);  		
-	    }			
-	}
-}
+  idFriendlyQuery: function(pQuery) {
+    var testQuery = pQuery;
+      if (!testQuery)
+        return testQuery;
+
+      if (testQuery instanceof Array) {
+        for (var i = 0; testQuery.length > i; i += 1) {
+          if (testQuery[i].hasOwnProperty('_id')) {
+            testQuery[i]["_id"] = new ObjectID(testQuery[i]["_id"]);
+          }
+        }
+      } else {
+        if (testQuery.hasOwnProperty('_id')) {
+          testQuery["_id"] = new ObjectID(testQuery["_id"]);
+        }
+      }
+
+      return testQuery;
+  },
+  checkIfValidQuery: function(pQuery) {
+    return (!query.hasOwnProperty(undefined) && Object.keys(pQuery).length > 0);
+  },
+  jsonParse: function(pProperty, pValue) {
+    return JSON.parse('{ "' + pProperty + '": "' + pValue + '" }');
+  },
+  dbResult: function(pErr, pRes, pResult, pResultType) {
+    if (pErr) {
+      logging.Error(pErr);
+      pRes.status(422).jsonp({"error": "Es ist ein Fehler aufgetreten beim Versuch sich mit der Datenbank zu verbinden!"});
+    }
+    else {
+      if (!pResult)
+        pRes.status(422).jsonp({"error": pResultType });
+      else
+        pRes.status(200).jsonp(pResult);
+      }
+  }
+};

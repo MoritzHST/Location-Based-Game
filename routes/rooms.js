5feaaf5025c022b63d8b00c8a81eb2e55c0b0f4f
Markus Bähr <markus.baehr@fh-stralsund.de>
2018-04-13 19:02:58 +0100

    added new routes, file splitting and renaming

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,24 +1,22 @@
 const operations = require('../mongodb/operations');
-const objects = require('../mongodb/objects');
 const handler = require('../mongodb/handler');
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 
 /* Global */
 
 /* GET */
-/* Find User(s) */
-router.get('/find/:collection', function(req, res) {
-    operations.findObject(req.params.collection, (handler.checkIfValidQuery(req.query) ? req.query : null), function(err, item) {
+/* Find Room(s) */
+router.get('/find/rooms', function(req, res) {
+    operations.findObject("rooms", (handler.checkIfValidQuery(req.query) ? req.query : null), function(err, item) {
         handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " existiert nicht.");
     });
 });
 
 /* POST */
-/* Insert User */
-router.post('/insert/:collection', function(req, res) {
+/* Insert Room */
+router.post('/insert/rooms', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-        operations.updateObject(req.params.collection, req.query, null, function(err, item) {
+        operations.updateObject("rooms", req.query, null, function(err, item) {
             handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " kann nicht hinzugefüt werden.");
         });
     } else {
@@ -28,14 +26,14 @@ router.post('/insert/:collection', function(req, res) {
     }
 });
 
-/* Update User */
-router.post('/update/:collection/:id', function(req, res) {
+/* Update Room */
+router.post('/update/rooms/:id', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-        operations.updateObject(req.params.collection, handler.idFriendlyQuery({
+        operations.updateObject("rooms", handler.idFriendlyQuery({
             _id : req.params.id
         }), req.query, function(err, item) {
             handler.dbResult(err, res, item, "Das Item " + req.params.id + " konnte nicht mit " + JSON.stringify(req.query).replace(/\"/g, '') + " geupdatet werden.");
-        })
+        });
     } else {
         res.status(422).jsonp({
             "error" : "Die übergebenen Parameter sind ungültig"
@@ -43,10 +41,10 @@ router.post('/update/:collection/:id', function(req, res) {
     }
 });
 
-/* Delete User(s) */
-router.post('/delete/:collection', function(req, res) {
+/* Delete Room(s) */
+router.post('/delete/rooms', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-        operations.deleteObjects(req.params.collection, req.query, function(err, item) {
+        operations.deleteObjects("rooms", req.query, function(err, item) {
             handler.dbResult(err, res, item, "Die Items mit den Eigenschaften " + JSON.stringify(req.query).replace(/\"/g, '') + " konnten nicht gelöscht werden.");
         });
     } else {
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const operations = require('../mongodb/operations');
+const handler = require('../mongodb/handler');
+const router = require('express').Router();
+
+/* Global */
+
+/* GET */
+/* Find User(s) */
+router.get('/find/users', function(req, res) {
+    operations.findObject("users", (handler.checkIfValidQuery(req.query) ? req.query : null), function(err, item) {
+        handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " existiert nicht.");
+    });
+});
+
+/* POST */
+/* Insert User */
+router.post('/insert/users', function(req, res) {
+    if (handler.checkIfValidQuery(req.query)) {
+        operations.updateObject("users", req.query, null, function(err, item) {
+            handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " kann nicht hinzugefüt werden.");
+        });
+    } else {
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
+    }
+});
+
+/* Update User */
+router.post('/update/users/:id', function(req, res) {
+    if (handler.checkIfValidQuery(req.query)) {
+        operations.updateObject("users", handler.idFriendlyQuery({
+            _id : req.params.id
+        }), req.query, function(err, item) {
+            handler.dbResult(err, res, item, "Das Item " + req.params.id + " konnte nicht mit " + JSON.stringify(req.query).replace(/\"/g, '') + " geupdatet werden.");
+        });
+    } else {
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
+    }
+});
+
+/* Delete User(s) */
+router.post('/delete/users', function(req, res) {
+    if (handler.checkIfValidQuery(req.query)) {
+        operations.deleteObjects("users", req.query, function(err, item) {
+            handler.dbResult(err, res, item, "Die Items mit den Eigenschaften " + JSON.stringify(req.query).replace(/\"/g, '') + " konnten nicht gelöscht werden.");
+        });
+    } else {
+        res.status(422).jsonp({
+            "error" : "Die übergebenen Parameter sind ungültig"
+        });
+    }
+});
+
+module.exports = router;

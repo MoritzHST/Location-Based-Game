13b487894c7a704b21a5419497cf88d86004213a
Moritz Wilke <moritz.wilke@fh-stralsund.de>
2018-04-14 09:25:59 +0200

    SignUp-View fixed -baseUrl in AJAX-Request removed -cleaned up stylesheet

--- public/javascripts/ajax-call.js
+++ public/javascripts/ajax-call.js
@@ -1,9 +1,15 @@
-const baseUrl = "admin/test/";
-
+/**
+ * Stellt AJAX-Request
+ * @param pUrl "Adresse, an die der Request gestellt werden soll"
+ * @param pMethod "POST/GET als Methode zur Interaktion"
+ * @param pObject "Objekt, welches an die Adresse übergeben wird"
+ * @param pCallback "Funktion die ausgeführt wird, wenn Request erfolgreich war"
+ * @param pFailCallback "Funktion die ausgeführt wird, wenn Request nicht erfolgreich war"
+ */
 function ajaxRequest(pUrl, pMethod, pObject, pCallback, pFailCallback) {
     $.ajax({
         type: pMethod,
-        url: baseUrl + pUrl + pObject,
+        url: pUrl + pObject,
         data: pObject,
         success: pCallback,
         error: pFailCallback

c0951eab082239ab9c1d3a5864cb996299e9cd26
Moritz Wilke <moritz.wilke@fh-stralsund.de>
2018-04-14 12:21:55 +0200

    SignUp-View improved -enabled / disabled states for sign up button

--- public/javascripts/sign-up.js
+++ public/javascripts/sign-up.js
@@ -9,12 +9,29 @@ function init() {
         let $textfieldName = $("#textfield-name-sign-up");
         const name = ($textfieldName.val() == "" || $textfieldName.val() == undefined) ? "" : "name=" + $textfieldName.val();
         ajaxRequest("insert/users?", "POST", name, redirectOnSuccess, displayFailureMessage);
+        $(this).addClass('disabled');
     });
 
     //Tabs setzen
     $(function () {
         $("#signup_signin_switch").tabs();
     });
+
+    //Enabled Disabled Status für Pflichtfeld AGB's setzen
+    const checkAGB = $("#checkAGB");
+
+    checkAGB.on("click", function () {
+        if (!this.checked) {
+            $("#btn-sign-up").addClass('disabled');
+
+        }
+        else {
+            $("#btn-sign-up").removeClass("disabled");
+        }
+    });
+
+    //Checkbox onload disabled
+    checkAGB.prop("checked", false);
 
 
 }
@@ -34,5 +51,8 @@ function displayFailureMessage(callbackObj) {
     setNodeHookFromFile($('head'), document.getElementById("sign-up-in-failure-box-hook"), "../partials/sign-up-in-failure-box/sign-up-in-failure-box.html", function (pObj) {
         document.getElementById("sign-up-in-failure-box-error-message").innerHTML = pObj.responseJSON.error;
     }, callbackObj);
+
+    //Fehler aufgetreten -> registrieren Button wieder aktivieren
+    $("#btn-sign-up").removeClass("disabled");
 
 }

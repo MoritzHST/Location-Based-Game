74ebd31f6a63db6ce26a1174fdea66324743f552
Markus Bähr <markus.baehr@fh-stralsund.de>
2018-04-14 17:17:29 +0100

    Fix: User name can only be updated if the name not already exists

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,23 @@ const router = require('express').Router();
 
 /* GET */
 /* Find User(s) */
-router.get('/find/users', function (req, res) {
-    operations.findObject("users", (handler.checkIfValidQuery(req.query) ? req.query : null), function (err, item) {
+router.get('/find/users', function(req, res) {
+    operations.findObject("users", (handler.checkIfValidQuery(req.query) ? req.query : null), function(err, item) {
         handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " existiert nicht.");
     });
 });
 
 /* POST */
 /* Insert User */
-router.post('/insert/users', function (req, res) {
+router.post('/insert/users', function(req, res) {
     const username = req.query.name;
 
-    operations.findObject("users", {"name": username}, function (err, item) {
+    operations.findObject("users", {
+        "name" : username.trim()
+    }, function(err, item) {
         if (item !== null) {
             res.status(422).jsonp({
-                "error": "Nutzername ist bereits vergeben"
+                "error" : "Nutzername ist bereits vergeben"
             });
             return;
         }
@@ -30,54 +32,66 @@ router.post('/insert/users', function (req, res) {
             return;
         }
 
-        //generiere zufälligen User-Token
+        // generiere zufälligen User-Token
         req.query.token = operations.generateToken();
 
         if (handler.checkIfValidQuery(req.query)) {
-            operations.updateObject("users", req.query, null, function (err, item) {
+            operations.updateObject("users", req.query, null, function(err, item) {
                 handler.dbResult(err, res, item, "Das Item " + JSON.stringify(req.query).replace(/\"/g, '') + " kann nicht hinzugefüt werden.");
             });
         } else {
             res.status(422).jsonp({
-                "error": "Die übergebenen Parameter sind ungültig"
+                "error" : "Die übergebenen Parameter sind ungültig"
             });
         }
     });
 });
 
 /* Update User */
-router.post('/update/users/:id', function (req, res) {
-    let username = req.query.name;
-    if (username !== undefined) {
-        const validity = handler.getUsernameValidity(username);
-        if (!validity.isValid) {
-            res.status(422).jsonp(validity.err);
-            return;
-        }
-    }
-
+router.post('/update/users/:id', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-        operations.updateObject("users", handler.idFriendlyQuery({
-            _id: req.params.id
-        }), req.query, function (err, item) {
-            handler.dbResult(err, res, item, "Das Item " + req.params.id + " konnte nicht mit " + JSON.stringify(req.query).replace(/\"/g, '') + " geupdatet werden.");
+
+        // prüfe, ob der eingegebene Name zulässig ist.
+        let username = req.query.name;
+        if (username !== undefined) {
+            const validity = handler.getUsernameValidity(username);
+            if (!validity.isValid) {
+                res.status(422).jsonp(validity.err);
+                return;
+            }
+        }
+
+        // prüfe, ob der eingegebene Name bereits verwendet wird.
+        operations.findObject("users", req.query, function(err, item) {
+            if (item && item._id !== req.params.id) {
+                res.status(422).jsonp("Es existiert bereits ein Benutzer mit diesem Namen.");
+            } else {
+                // wenn alle tests bestanden wurden, versuche die
+                // Benutzerinformationen zu updaten.
+
+                operations.updateObject("users", handler.idFriendlyQuery({
+                    _id : req.params.id
+                }), req.query, function(err, item) {
+                    handler.dbResult(err, res, item, "Das Item " + req.params.id + " konnte nicht mit " + JSON.stringify(req.query).replace(/\"/g, '') + " geupdatet werden.");
+                });
+            }
         });
     } else {
         res.status(422).jsonp({
-            "error": "Die übergebenen Parameter sind ungültig"
+            "error" : "Die übergebenen Parameter sind ungültig"
         });
     }
 });
 
 /* Delete User(s) */
-router.post('/delete/users', function (req, res) {
+router.post('/delete/users', function(req, res) {
     if (handler.checkIfValidQuery(req.query)) {
-        operations.deleteObjects("users", req.query, function (err, item) {
+        operations.deleteObjects("users", req.query, function(err, item) {
             handler.dbResult(err, res, item, "Die Items mit den Eigenschaften " + JSON.stringify(req.query).replace(/\"/g, '') + " konnten nicht gelöscht werden.");
         });
     } else {
         res.status(422).jsonp({
-            "error": "Die übergebenen Parameter sind ungültig"
+            "error" : "Die übergebenen Parameter sind ungültig"
         });
     }
 });

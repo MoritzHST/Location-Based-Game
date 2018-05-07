4d90047c9c2fa6990f7a9563426a97df6a768bcc
Moritz Wilke <moritz.wilke@fh-stralsund.de>
2018-05-07 09:47:11 +0200

    QR-Scanner implemented

--- public/partials/qr-scanner/javascripts/qr-scanner.js
+++ public/partials/qr-scanner/javascripts/qr-scanner.js
@@ -1,60 +1,63 @@
-let scanner;
-let cameraList;
-const qrUrl = "http://qr.service.fh-strasund.de/";
+var scanner;
+var cameraList;
 
-/**
- * initiates the scanner
- * register onCodeScanned as Callback for successful scans
- * initiates Cameralist-Object
- */
-function initQRScanner() {
-    scanner = new Instascan.Scanner({video: document.getElementById('preview'), mirror: false});
+function initScanner() {
+    scanner = new Instascan.Scanner(
+        {video: document.getElementById('preview'), mirror: false}
+    );
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
             scanner.start(cameras[0]);
             cameraList = new CameraList(cameras);
         } else {
-            console.error('Keine Kameras gefunden.');
+            console.error('No cameras found.');
         }
     }).catch(function (e) {
         console.error(e);
     });
     scanner.addListener('scan', onCodeScanned);
+
     document.getElementById("nextCam").onclick = function () {
         cameraList.startNextCam();
     }
 }
 
 /**
- * This function should be called when a code is scanned successfully
- * @param content Content written in the scanned code
+ * Callback der ausgeführt wird, wenn ein Code erfolgreich gescannt wurde
+ * @param content Inhalt des QR-Codes
  */
 function onCodeScanned(content) {
-    if (content.startsWith(qrUrl)) {
-        content.replace(qrUrl, "");
-        ajaxRequest()
-    }
+    ajaxRequest("find/scan", "GET", {"identifier": content}, function (obj) {
+        console.log(obj);
+    }, function (obj) {
+        console.log(obj);
+    });
 }
 
 /**
- * This Object wraps the camera-list that is called asynchronously and handles the camera-swap
- * @param cameras List of cameras that were found
- * @constructor  */
+ * Wrapper für das Kamera-Objekt
+ * @param cameras List der möglichen Kameras
+ * @constructor
+ */
 function CameraList(cameras) {
     this.cameras = cameras;
     this.currentCam = 0;
+
     /**
-     * Starts the next possible cam from the List.
+     * Startet die nächste mögliche Kamera
      */
     this.startNextCam = function () {
-        let stoppingCam = this.cameras[this.currentCam];
-        scanner.stop(stoppingCam);
+        var stoppingCam = this.cameras[this.currentCam];
+        //scanner.stop(stoppingCam);
+        console.log("stopped: " + stoppingCam.id);
+
         if (this.currentCam + 1 < this.cameras.length) {
             this.currentCam++;
         } else {
             this.currentCam = 0;
         }
-        let startingCam = this.cameras[this.currentCam];
+        var startingCam = this.cameras[this.currentCam];
         scanner.start(startingCam);
+        console.log("starting: " + startingCam.id);
     };
 }

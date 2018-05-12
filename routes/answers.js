df54c235420ac66a92d6a9bb33400d200a8e9cac
Moritz Wilke <moritz.wilke@fh-stralsund.de>
2018-05-12 13:51:59 +0200

    Possibility to post an answer to an question and receive an feedback whether its correct or not

---
+++ routes/answers.js
@@ -0,0 +1,39 @@
+const operations = require('../mongodb/operations');
+const router = require('express').Router();
+
+const gameCollection = require('../mongodb/collections').GAMES;
+
+/* Global */
+
+/* Post */
+/* Post Answer for an Game */
+router.post('/post/answer', function (req, res) {
+    let gameId = req.query.gameId;
+    let answer = req.query.answer;
+    operations.findObject(gameCollection, {_id: gameId}, function (err, item) {
+        //Ungültge Game-ID escapen
+        if (err || item === null) {
+            res.status(422).jsonp({
+                "error": "Es wurde kein Spiel mit der ID gefunden!"
+            });
+            return;
+        }
+        //Prüfen ob es eine Antwort für die Frage gibt, die den selben String hat und korrekt ist
+        for (let i in item.answers) {
+            if (item.answers.hasOwnProperty(i)) {
+                if (item.answers[i].answer === answer && item.answers[i].isCorrect) {
+                    res.status(200).jsonp({
+                        "msg": "Die Antwort ist richtig"
+                    });
+                    return;
+                }
+            }
+        }
+        //Es wurde keine richtige Antwort gefunden, also muss sie falsch sein
+        res.status(422).jsonp({
+            "error": "Die Antwort ist falsch!"
+        });
+    });
+});
+
+module.exports = router;

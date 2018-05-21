6aeeac583658a9ce4870041caa500f4381ede373
Jan Tiedemann <jan.tiedemann@fh-stralsund.de>
2018-05-21 17:20:44 +0200

    - refactoring

--- data/mongo_data.js
+++ data/mongo_data.js
@@ -2,24 +2,14 @@ const operations = require('../mongodb/operations');
 const logging = require('../helper/logging');
 const dbObjects = require('./objects');
 
-/**
- * fügt dem übergebenen Objekt eine zufällige GUID an, die in Mongo verwendet werden kann, und gibt das Objekt zurück
- * @param pObject Objekt, dem eine zufällige GUID angefügt werden soll
- * @returns {*} pObjekt mit angefügter GUID
- */
-function addRandomGUID(pObject) {
-    function getRandomId() {
-        let id = "";
-        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for (let i = 0; i < 12; i++)
-            id += possible.charAt(Math.floor(Math.random() * possible.length));
-
-        return id;
+function clearCollections() {
+    for (let collection of dbObjects.keys()) {
+        operations.createCollection(collection, function (err, db) {
+            if (!err) {
+                logging.Info("Collection gelöscht: " + db);
+            }
+        });
     }
-
-    pObject._id = getRandomId();
-    return pObject;
 }
 
 /**
@@ -29,42 +19,23 @@ function insertIntoDatabase() {
     for (let collection of dbObjects.keys()) {
         let objects = dbObjects.get(collection);
         objects.forEach(function (object) {
-                //Objekt wird zunächst in der Datenbank gesucht
-                operations.findObject(collection, object, function (err, item) {
-                    //Ist das Objekt nicht vorhanden, wird es hinzugefügt
-                    if (!item || err) {
-                        insertIntoCollection(collection, object);
+                //Objekt wird eingefügt/ersetzt
+                operations.updateObject(collection, null, object, function (err, result) {
+                    if (!err) {
+                        logging.Info(collection + " erstellt: " + result.value._id);
+                        object._id = result.value._id;
                     }
+                    else
+                        logging.Error(err);
                 });
             }
         );
     }
 }
 
-/**
- * Generiert für ein übergebenes Objekt eine GUID und prüft ob diese in der gegebenen Collection bereits verwendet wird.
- * Wenn ja, wird die Funktion erneut aufgerufen und eine neue GUID generiert, wenn nicht wird das Objekt eingefügt
- * @param pCollection
- * @param pObject
- */
-function insertIntoCollection(pCollection, pObject) {
-    pObject = addRandomGUID(pObject);
-    //Prüfe ob Objekt mit der id bereits in der Collection vorhanden ist
-    operations.findObject(pCollection, {_id: pObject._id}, function (findError, findItem) {
-        //Wenn nicht, füge neues Objekt ein
-        if (findError || !findItem) {
-            operations.updateObject(pCollection, pObject, null, function (err, result) {
-                if (!err)
-                    logging.Info(pCollection + " erstellt: " + result.value._id);
-                else
-                    logging.Error(err);
-            });
-        } else {
-            //Neuer Einfügeversuch
-            insertIntoCollection(pCollection, pObject);
-        }
-    });
+if (process.env.env === "development") {
+    //Lösche die Collections
+    clearCollections();
+    //Funktionsaufruf für das File
+    insertIntoDatabase();
 }
-
-//Funktionsaufruf für das File
-insertIntoDatabase();

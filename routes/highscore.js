284f47b31b743f6a2f258f54308f43af5d9051cd
Jan Tiedemann <jan.tiedemann@fh-stralsund.de>
2018-06-03 21:25:40 +0200

    - highscore route fertig gestellt

--- routes/highscore.js
+++ routes/highscore.js
@@ -7,7 +7,7 @@ const locationMappingCollection = require('../mongodb/collections').LOCATION_MAP
 const userCollection = require('../mongodb/collections').USERS;
 const eventHelper = require('../helper/event');
 
-const errorMessage = "Fehler beim auslesen der Missionsübersicht";
+const errorMessage = "Fehler beim auslesen der Highscores";
 
 /* Global */
 
@@ -17,7 +17,32 @@ const errorMessage = "Fehler beim auslesen der Missionsübersicht";
  */
 router.get('/score', async function (req, res) {
     logging.Entering("GET /score");
-    operations.findObject(userCollection, {});
+    operations.findObject(userCollection, null, function (err, items) {
+        items.sort(function (a, b) {
+            let ascore = a.score.score;
+            let bscore = b.score.score;
+            if (ascore < bscore) {
+                return 1;
+            }
+            if (ascore > bscore) {
+                return -1;
+            }
+            return 0;
+        });
+        let place = 1;
+        for (let index = 0; index < items.length; index++) {
+            let item = items[index];
+            item.token = undefined;
+            item.visits = undefined;
+            if (index !== 0 && item.score.score === items[index - 1].score.score) {
+                item.place = items[index - 1].place;
+            } else {
+                item.place = place;
+            }
+            place++;
+        }
+        handler.dbResult(err, res, items, errorMessage);
+    });
     logging.Leaving("GET /score");
 });
 

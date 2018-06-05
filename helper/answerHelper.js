3118f210e40c2a9229738df31244484f7fb0b510
Jan Tiedemann <jan.tiedemann@fh-stralsund.de>
2018-06-05 09:09:01 +0200

    - anpassungen der javaDocs

--- helper/answerHelper.js
+++ helper/answerHelper.js
@@ -76,19 +76,12 @@ function saveAnswer(pRequest, pState, pEvent, pGame) {
             }
         }
         userItem.score.games += 1;
+        userItem.score.locations = userItem.visits.length;
         if (pState === objects.GameStates.CORRECT) {
             userItem.score.score = userItem.score ? userItem.score.score + pGame.points : pGame.points;
         }
         // Score Connection aktualisieren
-        let ws = new WebSocket();
-        ws.on("connect", function (connection) {
-            connection.send(JSON.stringify({
-                userId: userItem._id,
-                score: eventHelper.formatScoreObject(pEvent, userItem.score)
-            }));
-            connection.close();
-        });
-        ws.connect("ws://127.0.0.1:3000/score");
+        broadcastScore(userItem, pEvent);
 
         operations.updateObject(userCollection, {
                 _id: userItem._id
@@ -117,9 +110,6 @@ function evaluateLocation(pRequest, pEvent) {
             if (userItem.visits.hasOwnProperty(i) && userItem.visits[i].location._id.toString() === mappingItem.location._id.toString()) {
                 //Status des Raumes aktualisieren
                 userItem.visits[i].state = analyzeVisits(userItem.visits[i], mappingItem);
-                if (userItem.visits[i].state === objects.RoomStates.VISITED) {
-                    userItem.score.locations += 1;
-                }
             }
         }
         operations.updateObject(userCollection, {
@@ -200,6 +190,19 @@ function findVisitByLocationIdentifier(pUserObj, pIdentifier) {
         }
     }
     logging.Leaving("findVisitByLocationIdentifier");
+}
+
+function broadcastScore(pUser, pEvent) {
+    // Score Connection aktualisieren
+    let ws = new WebSocket();
+    ws.on("connect", function (connection) {
+        connection.send(JSON.stringify({
+            userId: pUser._id,
+            score: eventHelper.formatScoreObject(pEvent, pUser.score)
+        }));
+        connection.close();
+    });
+    ws.connect("ws://127.0.0.1:3000/score");
 }
 
 module.exports = {

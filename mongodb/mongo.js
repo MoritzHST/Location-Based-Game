661a6f56adf554e0da782933be0ad620373abf10
Moritz Wilke <moritz.wilke@fh-stralsund.de>
2018-06-09 10:08:24 +0200

    Sonarqube Cleanup #7

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -28,9 +28,9 @@ if (process.env.conf !== "false") {
 
         for ( var key in Conf) {
             if (Conf.hasOwnProperty(key)) {
-                for (var line = 0; line < lines.length; line++) { // NOSONAR
-                    Conf[key] = extractValue(delimeters, lines, line, key) ? extractValue(delimeters, lines, line, key) : Conf[key]; // NOSONAR
-                } // NOSONAR
+                for (var line = 0; line < lines.length; line++) {
+                    Conf[key] = extractValue(delimeters, lines, line, key) ? extractValue(delimeters, lines, line, key) : Conf[key];
+                }
             }
         }
     }
